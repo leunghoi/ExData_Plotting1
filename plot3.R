88a355067b1e4321cd455e749845281d99a65ee8
leunghoi <hoifaileung@hushmail.com>
2015-09-09 23:43:36 -0400

    add plot 3

---
+++ plot3.R
@@ -0,0 +1,33 @@
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "power_consumption.zip")
+unzip("power_consumption.zip")
+
+hpc <- read.csv("household_power_consumption.txt", sep = ";")
+
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+select_date <- hpc$Date == as.Date("2/1/2007", format="%m/%d/%Y") | 
+    hpc$Date == as.Date("2/2/2007", format="%m/%d/%Y")
+
+hpc <- hpc[select_date,]
+
+hpc$Time <- strptime(paste(hpc$Date, hpc$Time, sep = " "), format = "%Y-%m-%d %H:%M:%S")
+hpc$Global_active_power <- as.numeric(gsub(",", "", hpc$Global_active_power))
+hpc$Sub_metering_1 <- as.numeric(gsub(",", "", hpc$Sub_metering_1))
+hpc$Sub_metering_2 <- as.numeric(gsub(",", "", hpc$Sub_metering_2))
+hpc$Sub_metering_3 <- as.numeric(gsub(",", "", hpc$Sub_metering_3))
+
+head(hpc, 100)
+
+
+png("plot3.png", height=480, width=480)
+
+plot(hpc$Time, hpc$Sub_metering_1, xlab = "", 
+     ylab="Energy sub metering", 
+     type="n")
+lines(hpc$Time, hpc$Sub_metering_1, col="black")
+lines(hpc$Time, hpc$Sub_metering_2, col="red")
+lines(hpc$Time, hpc$Sub_metering_3, col="blue")
+legend("topright", lwd=1, col=c("black", "red", "blue"),
+       legend = c("sub_metering_1", "sub_metering_2", "sub_metering_3"))
+
+dev.off()
